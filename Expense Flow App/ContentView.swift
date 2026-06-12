2efc95ab790df1ed4c822730c9773b83b0874947
khaleefahwaziri <khaleefahabubakar39@gmail.com>
2026-06-12 16:22:24 +0100

    Initial Commit

---
+++ Expense Flow App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Flow App
+//
+//  Created by Khalifa Waziri on 12/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Flow App/Expense_Flow_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Flow_AppApp.swift
+//  Expense Flow App
+//
+//  Created by Khalifa Waziri on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Flow_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
